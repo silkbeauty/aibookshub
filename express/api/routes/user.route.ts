9194dabc0d45d2e7f3a8831dbb80f5a30cd0d7d3
aiaccfin <aiaccfin@gmail.com>
2024-11-08 22:32:57 -0500

    update

--- express/api/routes/user.route.ts
+++ express/api/routes/user.route.ts
@@ -1,56 +1,41 @@
-require('dotenv').config();
-
-const express = require('express');
-const app = express();
 const { sql } = require('@vercel/postgres');
-
-const bodyParser = require('body-parser');
+const express = require('express');
+const router = express.Router();
 const path = require('path');
-
-// Create application/x-www-form-urlencoded parser
+const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '..', 'components', 'home.htm'));
+router.get('/uploadUser', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', '..', 'components', 'user_upload_form.htm'));
 });
 
-app.get('/about', function (req, res) {
-	res.sendFile(path.join(__dirname, '..', 'components', 'about.htm'));
-});
-
-app.get('/uploadUser', function (req, res) {
-	res.sendFile(path.join(__dirname, '..', 'components', 'user_upload_form.htm'));
-});
-
-app.post('/uploadSuccessful', urlencodedParser, async (req, res) => {
-	try {
-		await sql`INSERT INTO Users (Id, Name, Email) VALUES (${req.body.user_id}, ${req.body.name}, ${req.body.email});`;
-		res.status(200).send('<h1>User added successfully</h1>');
-	} catch (error) {
-		console.error(error);
-		res.status(500).send('Error adding user');
-	}
+router.post('/uploadSuccessful', urlencodedParser, async (req, res) => {
+    try {
+        await sql`INSERT INTO Users (Id, Name, Email) VALUES (${req.body.user_id}, ${req.body.name}, ${req.body.email});`;
+        res.status(200).send('<h1>User added successfully</h1>');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error adding user');
+    }
 });
 
 
-app.get('/allUsers', async (req, res) => {
-	try {
-		const users = await sql`SELECT * FROM Users;`;
-		if (users && users.rows.length > 0) {
-			let tableContent = users.rows
-				.map(
-					(user) =>
-						`<tr>
+router.get('/allUsers', async (req, res) => {
+    try {
+        const users = await sql`SELECT * FROM Users;`;
+        if (users && users.rows.length > 0) {
+            let tableContent = users.rows
+                .map(
+                    (user) =>
+                        `<tr>
                         <td>${user.id}</td>
                         <td>${user.name}</td>
                         <td>${user.email}</td>
                     </tr>`
-				)
-				.join('');
+                )
+                .join('');
 
-			res.status(200).send(`
+            res.status(200).send(`
                 <html>
                     <head>
                         <title>Users</title>
@@ -99,15 +84,13 @@ app.get('/allUsers', async (req, res) => {
                     </body>
                 </html>
             `);
-		} else {
-			res.status(404).send('Users not found');
-		}
-	} catch (error) {
-		console.error(error);
-		res.status(500).send('Error retrieving users');
-	}
+        } else {
+            res.status(404).send('Users not found');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error retrieving users');
+    }
 });
 
-app.listen(3001, () => console.log('Server ready on port 3001.'));
-
-module.exports = app;
+module.exports = router;
