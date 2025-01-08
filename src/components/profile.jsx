4a323b9cef70d24604a267dab8432dd79f8b605a
aiaccfin <aiaccfin@gmail.com>
2025-01-07 22:53:02 -0500

    update

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,9 +1,11 @@
 const ProfileContent = () => {
     return (
         <>
-            <div className="profile-content"><img src="/images/aibookshub-logo-removebg-preview.png" alt="profileImg" /></div>
-            {/* <div className="profile_name">books</div>
-            <div className="job">ai books hub</div> */}
+            <div className="profile_name">
+                <a href="/">
+                    <img src="/images/aibookshub-logo-removebg-preview.png" alt="profileImg" style={{ transform: 'scale(0.8)' }} />
+                </a>
+            </div>
         </>
     );
 };
